571181d58c015af5b37df4367b50dab6a3644252
Jose Lopez Renteria <joselukas24@gmail.com>
2024-04-12 19:44:55 -0500

    add App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import MainPage from "./pages/MainPage";
 import MarketPlace from "./pages/MarketPlace";
 import ProductPage from "./pages/ProductPage";
